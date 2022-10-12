d44445b07bfdf285635e6da48f2a6cd30175d88d
Anna <anna.spalek@icloud.com>
2022-10-12 13:03:44 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState} from 'react';
+import {Component, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 // class Slider extends Component {
@@ -52,6 +52,26 @@ const Slider = (props) => {
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
 
+    function logging() {
+        console.log('log!');
+    }
+
+    useEffect(() => {
+        console.log('effect');
+        document.title = `Slide: ${slide}`;
+
+        window.addEventListener('click', logging);
+
+        return () => {
+            window.removeEventListener('click', logging);
+        }
+
+    }, [slide]);
+
+    useEffect(() => {
+        console.log('autoplay');
+    }, [autoplay]);
+
     function changeSlide(i) {
         setSlide(slide => slide + i);
     }
@@ -83,8 +103,13 @@ const Slider = (props) => {
 
 
 function App() {
+    const [slider, setSlider] = useState(true);
+
   return (
-        <Slider/>
+        <>
+            <button onClick={() => setSlider(false)}>Click</button>
+            {slider ? <Slider/> : null}
+        </>
   );
 }
 
